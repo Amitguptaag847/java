99c9c6466aa357679be2afb9d2e78ec182089098
Amit Gupta <amitgupta.ag847@gmail.com>
2023-09-23 14:26:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("Output from java");
-        System.out.println("Hello");
+        System.out.println("Bye from Java");
     }
 }
